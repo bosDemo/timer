749c2437e98a1c900ffdde6b695a9dfdadc3a594
songguagnqian <ayw0165@dingtalk.com>
2019-01-24 16:28:06 +0800

    添加一个回参

--- src/main/java/com/example/controller/IdWord.java
+++ src/main/java/com/example/controller/IdWord.java
@@ -4,10 +4,11 @@ import com.example.utils.IdWorkerUtilss;
 
 public class IdWord {
     public static void main(String[] args) {
-
+        int a= 100;
         for (int i=1;i<100;i++){
             String idStr = IdWorkerUtilss.getIdStr();
             System.out.println(idStr);
+            System.out.println(idStr+a);
         }
 
     }
